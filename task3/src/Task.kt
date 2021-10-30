bb1410299178c1099e51bb326074315505618e8e
slampy97 <slampy97@gmail.com>
2021-10-30 14:26:46 +0300

    fix2

--- task3/src/Task.kt
+++ task3/src/Task.kt
@@ -1,14 +1,4 @@
-package com.h0tk3y.spbsu.kotlin.course.lesson1
 
-/*
- * Реализуйте функцию intersectRanges, возвращающую диапазон, являющийся пересечением двух
- * данных целочисленных диапазонов. Если данные диапазоны не пересекаются, возвращайте null.
- *
- * Например, пересечением дипазонов `1..10` и `5..20` является `5..10`,
- * а диапазоны `0..0` и `1..1` не пересекаются.
- * 
- * Для получения границ диапазонов используйте свойства `first` и `last`.
- */
 fun intersectRanges(rangeA: IntRange, rangeB: IntRange): IntRange? {
     if (rangeA.last < rangeB.first || rangeB.last < rangeA.first)
         return null
@@ -20,7 +10,7 @@ fun intersectRanges(rangeA: IntRange, rangeB: IntRange): IntRange? {
  * диапазонов, которые получаются из диапазона `outerRange`, если из него "вырезать" диапазон `cutout`.
  * (на самом деле, конечно же, "вырезать" из диапазона можно только его пересечение с другим диапазоном,
  *  а остальную часть другого диапазона следует отбросить)
- * 
+ *
  * Например, если из диапазона `0..10` вырезать `1..7`, получится два диапазона, `0..0` и `8..10`.
  * Если же из `0..10` вырезать `0..3`, то результатом будет только один диапазон `4..10`.
  */
@@ -31,19 +21,24 @@ fun cut(outerRange: IntRange, cutout: IntRange): List<IntRange> {
         result.add(outerRange)
         return result
     }
-    val check1: Int = if (intersection.first == outerRange.first) {1} else {0}
-    val check2: Int = if (intersection.last == outerRange.last) {10} else {100}
-    when(check1 + check2) {
-        101 -> {
+
+    val fst = intersection.first == outerRange.first
+    val snd = intersection.last == outerRange.last
+
+    when {
+        (fst && !snd) -> {
             result.add(intersection.last + 1 .. outerRange.last)
         }
-        10 -> {
+
+        (!fst && snd) -> {
             result.add(outerRange.first until intersection.first)
         }
-        100 -> {
+
+        (!fst && !snd) -> {
             result.add(outerRange.first .. intersection.first)
             result.add(intersection.last + 1 .. outerRange.last)
         }
     }
+
     return  result
 }
